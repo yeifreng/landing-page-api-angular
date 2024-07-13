663d7591291e3ff0df726ba314240fe91d32b716
Yeifreng <yeifrenjose@gmail.com>
2024-07-13 06:14:00 -0500

    creacion de los componentes contacto, productos y detalle de producto, asi mismo creamos el servicio para consumir la API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
