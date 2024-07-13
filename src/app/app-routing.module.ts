3727316f322624bf7e30eafebd40c290a73b49ef
Yeifreng <yeifrenjose@gmail.com>
2024-07-13 06:20:19 -0500

    se crea las rutas para poder navegar entre componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'products', component: ProductsComponent},
+  {path:'products/:id', component: ProductDetailComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'**', redirectTo:'', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
