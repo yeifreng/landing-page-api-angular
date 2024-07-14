f2d0872ffce22a713ffed36a9a8e63dd59aca641
Yeifreng <yeifrenjose@gmail.com>
2024-07-14 06:44:56 -0500

    Inyectamos el servicio en el componente productos para traernos los productos desde el TS

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { ApiService } from './../../services/api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+//array que recibira la lista de productos desed el servicio
+  productList: any[] = [];
+
+  //Inyectamos el servicio en el componente
+  constructor(private _apiService: ApiService){
+
+  }
+
+  //Al iniciar el componente, traera desde el servicio, la lista de productos y se le asignara al array productList
+  ngOnInit(): void {
+    this._apiService.getProducts().subscribe((data:any[]) => {
+      console.log(data);
+      this.productList = data;
+    })
+  }
+
+
 
 }
